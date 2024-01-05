421dcbb98c13b4c98a504ee5c406399c50dae80c
James <jcabs@Jamess-MacBook-Pro.local>
2024-01-05 22:21:52 +0000

    updates to playwright.yml file 11

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,8 +34,7 @@ export default defineConfig({
   workers: 1,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: 'html',
-  reporter: process.env.CI ? 'dot' : 'list',
+  reporter: [['list', { printSteps: true }]],
   
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
