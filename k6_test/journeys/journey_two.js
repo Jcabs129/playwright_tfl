b88170200adc8e1ca81fa0beb7dc4476a0e11d8f
James <jcabs@Jamess-MacBook-Pro.local>
2024-02-23 15:56:08 +0000

    new k6 journey - get BATCH

--- k6_test/journeys/journey_two.js
+++ k6_test/journeys/journey_two.js
@@ -1,30 +1,17 @@
-import http from 'k6/http';
-import { check } from 'k6';
+
 import { Httpx, Get } from 'https://jslib.k6.io/httpx/0.1.0/index.js';
 import { describe } from 'https://jslib.k6.io/expect/0.0.4/index.js';
 
 
 export const options = {
   stages: [
-    { duration: "5s", target: 1 },
+    { duration: "3s", target: 1 },
   ],
 }
 
-const baseURL = `https://dummy.restapiexample.com`
-
-const baseUrls = [
-  `${baseURL}/api/v1/employees`,
-  `${baseURL}/api/v1/employee/1`,
-]
+const session = new Httpx({ baseURL: 'https://rickandmortyapi.com/api' });
 
 export default function () {
-  baseUrls.forEach(function (url) {
-    const res = http.get(url);
-    check(res, {
-      [`${url}search status was 200`]: (r) => r.status === 200 
-    })
-  });
-
   describe('check status for ricky and morty api', (t) => {
     const responses = session.batch(
       [
@@ -32,9 +19,6 @@ export default function () {
         new Get('/location'),
         new Get('/episode'),
       ],
-      {
-        tags: { name: 'PublicCrocs' },
-      }
     );
 
     responses.forEach((response) => {
